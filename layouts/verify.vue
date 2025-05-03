713cc30cd9a4e9181c9ac6268445209d664936ae
ValentinWebDevY <valentinwebdevy@gmail.com>
2025-05-03 17:39:29 +0200

    add layout verify seo

--- layouts/verify.vue
+++ layouts/verify.vue
@@ -6,7 +6,33 @@
 </template>
 
 <script setup lang="ts">
+const route = useRoute()
 
+const title = computed(() => {
+  if (!route.meta.title)
+    return 'Les Douces Merveilles';
+
+  if (typeof route.meta.title === 'string') {
+    return route.meta.title;
+  }
+
+  if (typeof route.meta.title === 'function') {
+    return route.meta.title();
+  }
+
+  return 'Les Douces Merveilles';
+});
+
+const description = computed(() => route.meta.description || null);
+
+useSeoMeta({
+  title,
+  ogTitle: title,
+  twitterTitle: title,
+  description,
+  ogDescription: description,
+  twitterDescription: description,
+});
 </script>
 
 <style scoped>
